98cd82dfa812e3ea018509111be049b7cfaabcdf
jigss <jigshah7992@gmail.com>
2018-04-25 21:55:02 -0700

    first commit

---
+++ compare.py
@@ -0,0 +1,11 @@
+x = True
+y= False
+z = False
+
+if x or y and z:
+    print ('yes')
+else:
+    print('no')
+
+
+################
---
+++ if.py
@@ -0,0 +1,6 @@
+spam =42
+def eggs():
+    spam = 4
+    print("local "+ str(spam))
+print ('global '+ str(spam))
+eggs()
